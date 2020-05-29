e50ef9c31366a8aea1867d0501bbd133ba0e0b6e
nasvetil <nasvetil@ya.ru>
2020-05-29 23:58:39 +0300

    Add isImportant flag background-color

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import Context from '../context';
 
+import { makeStyles } from '@material-ui/styles';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
@@ -12,11 +13,18 @@ import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FlagIcon from '@material-ui/icons/Flag';
 
+const useStyles = makeStyles((theme) => ({
+  isImportant: {
+    backgroundColor: "lightblue"
+  }
+}));
+
 function TodoItem({ todo, index, onToggle, onToggleIsImportantTodo }) {
   const { removeTodo } = useContext(Context);
   const [checked, setChecked] = React.useState([0]);
   const [isImportant, setIsImportant] = React.useState(todo.isImportant);
-  const classes = [];
+  const classes = useStyles();
+  const classesText = [];
 
   const handleToggle = (value) => () => {
     onToggle(todo.id);
@@ -38,7 +46,7 @@ function TodoItem({ todo, index, onToggle, onToggleIsImportantTodo }) {
   };
 
   if (todo.completed) {
-    classes.push('list__item--completed');
+    classesText.push('list__item--completed');
   }
 
   return (
@@ -47,7 +55,8 @@ function TodoItem({ todo, index, onToggle, onToggleIsImportantTodo }) {
       role={undefined}
       dense
       button
-      onClick={handleToggle(index)}>
+      onClick={handleToggle(index)}
+      className={isImportant ? classes.isImportant : null}>
 
       <ListItemIcon>
         <Checkbox
@@ -61,7 +70,7 @@ function TodoItem({ todo, index, onToggle, onToggleIsImportantTodo }) {
 
       <ListItemText
         id={`checkbox-list-label-${index}`}
-        primary={<span className={classes.join(' ')}>{index + 1}. {todo.title}</span>}
+        primary={<span className={classesText.join(' ')}>{index + 1}. {todo.title}</span>}
       />
 
       <ListItemSecondaryAction>
